148507096877aa8205a8885bd1a0b7ab78d9638b
Clément Dauvilliers <clement.dauvilliers@grenoble-inp.org>
2021-12-12 01:05:20 +0100

    Filtering of the edges based on the relative weights.

--- scripts/view_graph.py
+++ scripts/view_graph.py
@@ -8,6 +8,7 @@ import networkx as ntx
 import pyvis
 import os
 import json
+import numpy as np
 import matplotlib.pyplot as plt
 from networkx.readwrite import json_graph
 
@@ -37,12 +38,16 @@ def filter_nodes(ntx_graph, min_weight=1000):
             ntx_graph.remove_node(node_name)
 
 
-def build_graph(ntx_graph, network):
+def build_graph(ntx_graph, network, edges_proportion=0.05):
     """
     Builds the pyvis graph from the NetworkX graph.
     :param ntx_graph: networkx graph to translate
     :param network: PyVis graph to fill with the data from
         ntx_graph
+    :param edges_proportion: float between 0 and 1. Proportion
+        of the edges coming out of each node to be included in
+        the PyVis graph. For example, 0.2 means only the 20%
+        of edges with the largest weight will be displayed.
     :return: the PyVis version of the graph.
     """
     # Step 1: add the nodes
@@ -52,11 +57,19 @@ def build_graph(ntx_graph, network):
         # name, and value is the number of speakers contained in the node
         network.add_node(node_name, label=node_name, value=node['weight'])
     # Step 2: add the edges
-    for edge_data in ntx_graph.edges.data():
-        node_a, node_b, props = edge_data
-        weight = props['weight']
-        if weight > 500:
-            network.add_edge(node_a, node_b, value=weight)
+    nodes_data = ntx_graph.nodes.data()
+    for node in ntx_graph.nodes:
+        # Retrieves the data for each edge coming out of node
+        out_weights = [ntx_graph.get_edge_data(node, s)['weight'] / nodes_data[s]['weight']
+                       for s in ntx_graph.successors(node)
+                       ]
+        # Sets a minimum weight. All edges coming out of node
+        # whose weight is inferior to that value will NOT be added
+        # to the PyVis graph.
+        min_accepted_weight = np.quantile(out_weights, 1 - edges_proportion)
+        for k, succ in enumerate(ntx_graph.successors(node)):
+            if out_weights[k] >= min_accepted_weight:
+                network.add_edge(node, succ, value=out_weights[k])
 
 
 if __name__ == "__main__":
@@ -66,7 +79,7 @@ if __name__ == "__main__":
     network = pyvis.network.Network(height='750px', width='100%', directed=True)
     build_graph(ntx_graph, network)
 
-    network.barnes_hut(spring_length=600, spring_strength=4, overlap=1)
+    network.barnes_hut(central_gravity=0.3, spring_strength=0.01, spring_length=50)
     network.show_buttons(filter_=['physics'])
 
     network.show('nt.html')
